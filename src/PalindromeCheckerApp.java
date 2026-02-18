acfee9c14a459f602e66c87ede0f0c7e5d96c250
NIKHILBHUSHAN <nikhilbhushan95824@gmail.com>
2026-02-18 21:07:26 +0530

    UC1: Application Entry & Welcome Message

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,2 +1,13 @@
+class UseCase1{
+    void display(){
+        System.out.println("Welcome to the Palindrome Checker Management System");
+        System.out.println("Version:1.0");
+        System.out.println("System Initialized Successfully");
+    }
+}
 public class PalindromeCheckerApp {
+    public static void main(String[] args) {
+        UseCase1 u1=new UseCase1();
+        u1.display();
+    }
 }
